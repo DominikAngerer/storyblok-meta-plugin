22e28984958c535c0ad6673818cfe9ae118ea02d
Dominik Angerer <dominikangerer1@gmail.com>
2017-02-21 10:10:12 +0100

    removes fields which should be set globally

--- src/Plugin.vue
+++ src/Plugin.vue
@@ -17,11 +17,6 @@
       <span class="uk-text-muted form__hint">The title of your object.</span>
   </div>
   <div class="uk-margin-small-bottom">
-      <span class="form__topic">OG Site Name</span> 
-      <input v-model="model.og_site_name" class="uk-width-1-1" type="text" name="og_site_name">
-      <span class="uk-text-muted form__hint">The name which should be displayed for the overall site</span>
-  </div>
-  <div class="uk-margin-small-bottom">
       <span class="form__topic">OG Description</span> 
       <input v-model="model.og_description" class="uk-width-1-1" type="text" name="og_description">
       <span class="uk-text-muted form__hint">A one to two sentence description of your object.</span>
@@ -37,11 +32,6 @@
       <span class="form__topic">Twitter Title</span> 
       <input v-model="model.twitter_title" class="uk-width-1-1" type="text" name="twitter_title">
       <span class="uk-text-muted form__hint">The title of your object.</span>
-  </div>
-  <div class="uk-margin-small-bottom">
-      <span class="form__topic">Twitter Site</span> 
-      <input v-model="model.twitter_site" class="uk-width-1-1" type="text" name="twitter_site">
-      <span class="uk-text-muted form__hint">The name which should be displayed for the overall site</span>
   </div>
   <div class="uk-margin-small-bottom">
       <span class="form__topic">Twitter Description</span> 
@@ -67,7 +57,6 @@ export default {
         og_title: '',
         og_description: '',
         og_image: '',
-        og_site_name: 'Your site name (e.g Storyblok)',
         twitter_title: '',
         twitter_description: '',
         twitter_image: ''
@@ -75,7 +64,7 @@ export default {
     }
   },
   events: {
-    'plugin:created': function() {
+  'plugin:created': function() {
       if(this.$store && this.$store.model && this.$store.model.story){
         this.title = this.title ? this.title : this.$store.model.story.name
         this.og_title = this.og_title ? this.og_title : this.$store.model.story.name
